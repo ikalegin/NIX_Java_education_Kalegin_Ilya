90d30d435a048a2c55785c0bb1902332a85dd3de
ikalegin <i.kalehin@student.csn.khai.edu>
2021-10-21 23:29:06 +0300

    Test.java added to feature_branch
    Test.java added to feature_branch with change on string #2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,14 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Java");
+<<<<<<< HEAD
+        System.out.println("Java education");
+=======
+        System.out.println("education");
+        // hello world
+<<<<<<< HEAD
+>>>>>>> 8ff1b8a (Test.java added to feature_branch with change on string #2)
+=======
+        System.out.println("DICT");
+>>>>>>> 2995b2a (Test.java added to feature_branch with change on string #3)
     }
 }
